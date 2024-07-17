2d813631ef6d6ff3c964bafc3a49933f0c768edd
Falilou-MFD <falilou1999@gmail.com>
2024-07-17 22:32:14 +0000

    Backend fix

--- projet_meteo/settings.py
+++ projet_meteo/settings.py
@@ -12,6 +12,10 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 from pathlib import Path
 
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -37,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'weather_app'
 ]
 
 MIDDLEWARE = [
--- weather_app/views.py
+++ weather_app/views.py
@@ -2,17 +2,17 @@ import datetime
 
 import requests
 from django.shortcuts import render
-
+import os
 # Create your views here.
 def index(request):
-    #API_KEY = open("../projet_meteo/API_KEY", "r").read()
-    API_KEY = "3c7ca3a08e9a3455816efeb6a7c863bf"
-    current_weather_url = "https://api.openweathermap.org/data/2.5/weather?q={}&appid={}"
-    forecast_url = "https://api.openweathermap.org/data/2.5/onecall?lat={}&lon={}exclude=current,minutely,hourly,alerts&appid={}"
-
+    API_KEY = os.getenv("API_KEY")
+    base_url = "https://api.openweathermap.org/data/2.5"
+    current_weather_url = f"{base_url}/weather?q={{}}&appid={{}}"
+    forecast_url = f"{base_url}/onecall?lat={{}}&lon={{}}&exclude=current,minutely,hourly,alerts&appid={{}}"
+   
     if request.method =="POST":
-        city1 = request.POST["city1"]
-        city2 = request.POST["city2, None"]
+        city1 = request.POST.get("city1")
+        city2 = request.POST.get("city2", None)
 
         weather_data1, daily_forecasts1 = fetch_weather_and_forecast(city1, API_KEY, current_weather_url, forecast_url)
         if city2:
@@ -27,10 +27,10 @@ def index(request):
             "daily_forecasts2": daily_forecasts2
 
         }
-        return render(request, "lien_html", context)
+        return render(request, "lien_html.html", context)
     else:
         
-        return render(request, "lien_html")
+        return render(request, "lien_html.html")
 
 
 def fetch_weather_and_forecast(city, api_key, current_weather_url, forecast_url):
@@ -45,12 +45,12 @@ def fetch_weather_and_forecast(city, api_key, current_weather_url, forecast_url)
         "icon": response["weather"][0]["icon"]
     }
 
-    daily.forecasts = []
+    daily_forecasts = []
     for daily_data in forecast_response["daily"][:5]:
         daily_forecasts.append({
             "day": datetime.datetime.fromtimestamp(daily_data["dt"]).strftime("%A"),
-            "min_temp": round(daily_data["temp"]["min"] -273,15, 2),
-            "max_temp": round(daily_data["temp"]["max"] -273,15, 2),
+            "min_temp": round(daily_data["temp"]["min"] -273.15, 2),
+            "max_temp": round(daily_data["temp"]["max"] -273.15, 2),
             "description": daily_data["weather"][0]["description"],
             "icon": daily_data["weather"][0]["icon"]
         })
