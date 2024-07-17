abd19bab4edd05912bd04af654a6ae1eabb75914
Falilou-MFD <falilou1999@gmail.com>
2024-07-17 15:55:18 +0000

    backend fix API_KEY

--- weather_app/views.py
+++ weather_app/views.py
@@ -5,7 +5,8 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    API_KEY = open("../projet_meteo/API_KEY", "r").read()
+    #API_KEY = open("../projet_meteo/API_KEY", "r").read()
+    API_KEY = "3c7ca3a08e9a3455816efeb6a7c863bf"
     current_weather_url = "https://api.openweathermap.org/data/2.5/weather?q={}&appid={}"
     forecast_url = "https://api.openweathermap.org/data/2.5/onecall?lat={}&lon={}exclude=current,minutely,hourly,alerts&appid={}"
 
